456db83f3f4b2d894ab891e3b3f3eeccacd242d9
Metin Çelik <metincelik88@gmail.com>
2026-08-24 20:05:21 +0200

    Add choice picker

--- src/ui/general_settings.cpp
+++ src/ui/general_settings.cpp
@@ -24,6 +24,7 @@
 #include "clisp.h"
 #include "dev.h"
 #include "general_settings.h"
+#include "gui.h"
 #include "id.h"
 #include "jwindow.h"
 #include "lisp.h"
@@ -50,7 +51,6 @@ constexpr std::array<const char *, PLAYER_SKIN_COUNT> skin_label_symbols = {
     "skin_standard", "skin_blue",   "skin_yellow", "skin_fire",   "skin_olive", "skin_pink",
     "skin_darkblue", "skin_purple", "skin_africa", "skin_gold",   "skin_land"};
 
-constexpr int skin_picker_rows = 6;
 constexpr int skin_preview_width = 54;
 constexpr int skin_preview_height = 58;
 
@@ -102,36 +102,6 @@ class language_picker : public pick_list
             scroll_event(0, screen);
             note_new_current(screen, inm, cur_sel);
         }
-    }
-};
-
-class skin_picker : public pick_list
-{
-    int minimum_item_width;
-
-  public:
-    skin_picker(int X, int Y, int ID, char **labels, int selected, int MinimumItemWidth, ifield *Next)
-        : pick_list(X, Y, ID, skin_picker_rows, labels, PLAYER_SKIN_COUNT, selected, Next, nullptr, false),
-          minimum_item_width(MinimumItemWidth)
-    {
-    }
-
-    int item_width() override
-    {
-        return std::max(minimum_item_width, pick_list::item_width());
-    }
-
-    void note_new_current(image *screen, InputManager *inm, int x) override
-    {
-        wm->PushMessage(id, this);
-    }
-
-    void set_x(int x, image *screen) override
-    {
-        const int previous = get_selection();
-        pick_list::set_x(x, screen);
-        if (get_selection() != previous)
-            wm->PushMessage(id, this);
     }
 };
 
@@ -246,7 +216,7 @@ void show_general_settings()
     while (true)
     {
         std::array<char *, languages.size()> labels;
-        std::array<char *, skin_label_symbols.size()> skin_labels;
+        std::vector<std::string> skin_labels;
         int selected = 0;
         int longest_label = 0;
         const std::string effective_language = settings.GetEffectiveLanguage();
@@ -257,12 +227,9 @@ void show_general_settings()
             if (effective_language == languages[index].value)
                 selected = static_cast<int>(index);
         }
-        int longest_skin_label = 0;
-        for (size_t index = 0; index < skin_label_symbols.size(); ++index)
-        {
-            skin_labels[index] = const_cast<char *>(symbol_str(skin_label_symbols[index]));
-            longest_skin_label = std::max(longest_skin_label, text_width(skin_labels[index]));
-        }
+        skin_labels.reserve(skin_label_symbols.size());
+        for (const char *label_symbol : skin_label_symbols)
+            skin_labels.emplace_back(symbol_str(label_symbol));
 
         const int font_height = wm->font()->Size().y;
         const int padding = 8;
@@ -270,27 +237,29 @@ void show_general_settings()
         const int label_y = padding / 2;
         const int picker_y = label_y + font_height + 3;
         const int language_natural_width = longest_label + padding * 2 + 8;
-        const int skin_natural_width = longest_skin_label + skin_preview_width + padding * 3 + 20;
-        const int client_width = std::max({settings.big_font ? 260 : 220, language_natural_width, skin_natural_width});
-        const int language_picker_width = client_width - padding * 2;
-
         const int language_picker_bottom = picker_y + static_cast<int>(languages.size()) * (font_height + 1) + 4;
         const int skin_label_y = language_picker_bottom + 6;
         const int skin_picker_y = skin_label_y + font_height + 3;
+        choice_picker *skin = new choice_picker(content_x, skin_picker_y, ID_PLAYER_SKIN_PICKER, skin_labels,
+                                                settings.player_skin, nullptr);
+        int skin_x1, skin_y1, skin_x2, skin_y2;
+        skin->area(skin_x1, skin_y1, skin_x2, skin_y2);
+        const int skin_control_width = skin_x2 - skin_x1 + 1;
+        const int skin_natural_width = skin_control_width + skin_preview_width + padding * 3;
+        const int client_width = std::max({settings.big_font ? 260 : 220, language_natural_width, skin_natural_width});
+        const int language_picker_width = client_width - padding * 2;
+
         const int preview_x = client_width - padding - skin_preview_width;
-        const int skin_picker_width = preview_x - content_x - padding - 12;
 
         skin_preview *preview = new skin_preview(preview_x, skin_picker_y, nullptr);
-        skin_picker *skin = new skin_picker(content_x, skin_picker_y, ID_PLAYER_SKIN_PICKER, skin_labels.data(),
-                                            settings.player_skin, skin_picker_width - 4, preview);
+        skin->next = preview;
         info_field *skin_label =
             new info_field(content_x, skin_label_y, ID_NULL, symbol_str("player_skin"), skin);
         language_picker *picker = new language_picker(content_x, picker_y, ID_LANGUAGE_PICKER, labels.data(), selected,
                                                       language_picker_width - 4, skin_label);
         info_field *fields = new info_field(content_x, label_y, ID_NULL, symbol_str("language"), picker);
 
-        const int skin_picker_bottom = skin_picker_y + skin_picker_rows * (font_height + 1) + 4;
-        const int content_bottom = std::max(skin_picker_bottom, skin_picker_y + skin_preview_height) + padding;
+        const int content_bottom = std::max(skin_y2 + 1, skin_picker_y + skin_preview_height) + padding;
         const ivec2 client_size(client_width, content_bottom);
         const ivec2 total_size = client_size + ivec2(Jwindow::left_border() + Jwindow::right_border(),
                                                      Jwindow::top_border() + Jwindow::bottom_border());
